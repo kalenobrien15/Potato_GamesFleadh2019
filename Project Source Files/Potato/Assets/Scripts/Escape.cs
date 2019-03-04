aef14d733290678093a77976f09b4391bf0736dc
kalenobrien15 <kalobrienz@gmail.com>
2019-03-04 16:09:50 +0000

    SourceFiles

---
+++ Project Source Files/Potato/Assets/Scripts/Escape.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Escape : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		}
+	}
+
